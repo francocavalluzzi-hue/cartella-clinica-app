ac7c0abcce07f7270065a79e3ad468b049a1895f
frankcava5000 <frankcava50@gmail.com>
2026-03-14 21:42:55 +0100

    feat: nuova funzionalità

--- app/patients/[id]/documents/page.tsx
+++ app/patients/[id]/documents/page.tsx
@@ -249,7 +249,7 @@ export default function DocumentsPage() {
     // Nome e Cognome del paziente (stima posizione)
     if (patient) {
       p1.drawText(`${patient.name || ""} ${patient.surname || ""}`, {
-        x: 182, y: H - 195, size: fs, font, color: black
+        x: 182, y: H - 215, size: fs, font, color: black
       })
     }
 
